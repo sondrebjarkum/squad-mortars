92243f4f96e652617716cc14fc0d1868d59a61e7
Sondre Bjarkum <erdnos@yahoo.no>
2024-05-25 12:52:06 +0200

    cleanup

--- app/main.js
+++ app/main.js
@@ -1,17 +1,10 @@
 import './styles/style.css'
-// import './components/select.js'; 
 import html from "./helpers/html.js"
 import App from './app.js'
-// import { setupMap } from './map.js'
 
 document.querySelector('#app').innerHTML = html`
     <div class="container">
       <div id="map"></div>
-      <sq-select>
-        <option value="anvil">Anvil</option>
-        <option value="chora">Chora</option>
-      </sq-select>
     </div>
 `
 App.init()
-// setupMap()
